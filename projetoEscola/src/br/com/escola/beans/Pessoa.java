484d0d0804eabe4d5d165fa609a5fcf13efffb53
teste <Gaaskdoa@gmail.com>
2020-10-20 19:59:06 -0300

    Projeto escola finalizado

---
+++ projetoEscola/src/br/com/escola/beans/Pessoa.java
@@ -0,0 +1,17 @@
+
+package br.com.escola.beans;
+
+public abstract class Pessoa {
+    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
